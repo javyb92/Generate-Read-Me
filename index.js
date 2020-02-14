e798c9f5da5131513b160362691682c184f2553d
jab_tx <javier.a.baragan92@gmail.com>
2020-02-14 06:10:06 -0600

    axios call to github works

--- index.js
+++ index.js
@@ -1,65 +1,71 @@
 const fs = require("fs");
 const path = require("path");
+const inquirer = require("inquirer")   
+const axios = require("axios");
 
-var inquirer = require("inquirer")    
-    
+    //Questions asked with prompt of inquirer
     const questions = [
         {
             type: "input",
             message:"What is your GitHub Username?",
             name:"userName"
         },
-        {
-            type: "input",
-            message:"What is your email?",
-            name:"userEmail"
-        },
-        {
-            type: "input",
-            message:"What is the title of your project?",
-            name:"projectTitle"
-        },
-        {
-            type: "input",
-            message:"How would you describe your project?",
-            name:"projectDescription"
-        },
-        {
-            type: "input",
-            message:"Who is this project for and how would they use it?",
-            name:"projectUsage"
-        },
-        {
-            type: "input",
-            message:"How do you install the project?",
-            name:"projectInstallation"
-        },
-        {
-            type: "input",
-            message:"What licenses did you use for the project?",
-            name:"projectLicenses"
-        },
-        {
-            type: "input",
-            message:"How would you test the project?",
-            name:"projectTesting"
-        },
+        // {
+        //     type: "input",
+        //     message:"What is your email?",
+        //     name:"userEmail"
+        // },
+        // {
+        //     type: "input",
+        //     message:"What is the title of your project?",
+        //     name:"projectTitle"
+        // },
+        // {
+        //     type: "input",
+        //     message:"How would you describe your project?",
+        //     name:"projectDescription"
+        // },
+        // {
+        //     type: "input",
+        //     message:"Who is this project for and how would they use it?",
+        //     name:"projectUsage"
+        // },
+        // {
+        //     type: "input",
+        //     message:"How do you install the project?",
+        //     name:"projectInstallation"
+        // },
+        // {
+        //     type: "input",
+        //     message:"What licenses did you use for the project?",
+        //     name:"projectLicenses"
+        // },
+        // {
+        //     type: "input",
+        //     message:"How would you test the project?",
+        //     name:"projectTesting"
+        // },
 
 
     ]
 
-        
+//init function at startup       
 function init() {
-    inquirer
-        .prompt(questions)
-        .then(function(answers){
-            console.log(answers.userName)
-            writeToFile('README.md', answers.userName)
-        })
-}
-        
-function writeToFile(fileName, data) {
-    fs.writeFileSync(path.join(process.cwd(), fileName), data)
-}
+    inquirer.prompt(questions)
+    
+.then(function ({ userName }){
+    const queryUrl = `https://api.github.com/users/${userName}/repos?per_page=100`;
+    
+    axios.get(queryUrl).then(function(data) {
+          console.log(data);
+        });
+})
+};
 
+// //Write to file, 
+// function writeToFile(fileName, data) {
+//     fs.writeFileSync(path.join(process.cwd(), fileName), data)
+// }
+
+// init starts the app
 init();
