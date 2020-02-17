6a7086a5da078f35b271c94b3a2aa0f9cd947d93
jab_tx <javier.a.baragan92@gmail.com>
2020-02-17 03:37:03 -0600

    added readme template, made inquirer  answers  written to ReadME and work with axios

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-javyb92
+
+    # 
+
+    ## About The Project
+    
+    
+    ### Built With
+    
+
+    ### Installation
+    
+    
+    ## Usage
+    
+    
+    ## License
+    
+
+    ## Contact
+    
+    Github Link:
+    
+    Email Link: 
+    
+    Project Link:
+    
+   
+   
+    
--- index.js
+++ index.js
@@ -1,72 +1,113 @@
 const fs = require("fs");
-const path = require("path");
+// const path = require("path");
 const inquirer = require("inquirer")   
-const axios = require("axios");
+const axios = require("axios")
+const util = require("util");
 
     //Questions asked with prompt of inquirer
-    const questions = [
-        {
-            type: "input",
-            message:"What is your GitHub Username?",
-            name:"userName"
-        },
-        // {
-        //     type: "input",
-        //     message:"What is your email?",
-        //     name:"userEmail"
-        // },
-        // {
-        //     type: "input",
-        //     message:"What is the title of your project?",
-        //     name:"projectTitle"
-        // },
-        // {
-        //     type: "input",
-        //     message:"How would you describe your project?",
-        //     name:"projectDescription"
-        // },
-        // {
-        //     type: "input",
-        //     message:"Who is this project for and how would they use it?",
-        //     name:"projectUsage"
-        // },
-        // {
-        //     type: "input",
-        //     message:"How do you install the project?",
-        //     name:"projectInstallation"
-        // },
-        // {
-        //     type: "input",
-        //     message:"What licenses did you use for the project?",
-        //     name:"projectLicenses"
-        // },
-        // {
-        //     type: "input",
-        //     message:"How would you test the project?",
-        //     name:"projectTesting"
-        // },
+const writeToFileAsync = util.promisify(fs.writeFile);
+function promptUser(){
+    return inquirer.prompt([
+            {
+                type: "input",
+                message:"What is your GitHub Username?",
+                name:"userName"
+             },
+            {
+                type: "input",
+                message:"What is your email?",
+                name:"userEmail"
+            },
+            {
+                type: "input",
+                message:"What is the title of your project?",
+                name:"projectTitle"
+            },
+            {
+                type: "input",
+                message:"How would you describe your project?",
+                name:"projectDescription"
+            },
+            {   
+                type: "input",
+                message:"How do you install the project?",
+                name:"projectInstallation"
+            },
+            {   
+                type: "input",
+                message:"What is the project built with?",
+                name:"projectAssets"
+            },
+            {
+                type: "input",
+                message:"What licenses did you use for the project?",
+                name:"projectLicenses"
+            },
+            {
+                type: "input",
+                message:"How would you test the project?",
+                name:"projectTesting"
+            },
+    ]);
+}
 
 
-    ]
+//Insert answers in preformatted ReadMe.md file
+function generateReadMe(answers){
+    return `
+    # ${answers.projectTitle}
+
+    ## About The Project
+    ${answers.projectDescription}
+    
+    ### Built With
+    ${answers.projectAssets}
+
+    ### Installation
+    ${answers.projectInstallation}
+    
+    ## Usage
+    
+    
+    ## License
+    ${answers.projectLicenses}
+
+    ## Contact
+    
+    Github Link:
+    
+    Email Link: ${answers.userEmail}
+    
+    Project Link:
+    
+   
+   
+    `}
+
 
 //init function at startup       
-function init() {
-    inquirer.prompt(questions)
-    
-.then(function ({ userName }){
-    const queryUrl = `https://api.github.com/users/${userName}/repos?per_page=100`;
-    
-    axios.get(queryUrl).then(function(data) {
-          console.log(data);
-        });
-})
-writeToFile()
-};
+async function init() {
+    try{
 
-// //Write to file, 
-// function writeToFile(fileName, data) {
-//     fs.writeFileSync(path.join(process.cwd(), fileName), data)
-// }
+        const answers = await promptUser();
+        console.log(answers)
+        const newReadMe = generateReadMe(answers);
+        await writeToFileAsync ("README.md", newReadMe);
+        
+        const queryUrl = `https://api.github.com/users/${answers.userName}/repos?per_page=100`;
+        axios.get(queryUrl).then(function(data) {
+        console.log(data);
+    });  
 
-// init starts the app
+}
+    catch(err){
+        console.log(err)
+        inquirer.prompt(answers)
+}
+
+
+
+}
+
+// starts the app
 init();
